f521d026ec1eecfb7b8fae63cc3bb8af5873d750
K0ai <khoachess6a5@gmail.com>
2026-07-08 22:20:44 +0700

    Mồm di chuyển
    ayayayayay

---
+++ mồm.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("move_left_catching_game", "move_right_catching_game")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
