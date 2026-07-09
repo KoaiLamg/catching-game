f78f4e3b5d569e9b99a1f396ea7e4ba505100fdc
Bloom <141619379+KoaiLamg@users.noreply.github.com>
2026-07-09 14:38:12 +0700

    Tạo bộ rơi ngẫu nhiên

--- nho.gd
+++ nho.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-
+@export var toc_do_roi :int = 100
 
 func _process(delta: float) -> void:
-	position.y+=100*delta
+	position.y+=toc_do_roi*delta
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("Entered")
