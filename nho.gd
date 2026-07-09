0fc5124dcb99c1003b7032ce13de3cd2ac7ad7c2
Bloom <141619379+KoaiLamg@users.noreply.github.com>
2026-07-09 14:33:44 +0700

    Sửa sửa chữa chữa

--- nho.gd
+++ nho.gd
@@ -2,15 +2,9 @@ extends Area2D
 
 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y+=100*delta
 
-
-
-
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	print("Entered")
 	queue_free()# Replace with function body.
