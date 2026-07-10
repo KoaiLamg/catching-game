4e654ff3bcaa8b0b078230f0df684333a2bc6536
Bloom <141619379+KoaiLamg@users.noreply.github.com>
2026-07-10 08:51:01 +0700

    change

--- nho.gd
+++ nho.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
 @export var toc_do_roi :int = 100
-
+var count=0
 func _process(delta: float) -> void:
 	position.y+=toc_do_roi*delta
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("Entered")
 	queue_free()# Replace with function body.
+	
